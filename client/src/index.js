f51245cc513ad60ab7abd3fd60c7e882ecf686e4
Jonathan Coleman <jcoleman3307@gmail.com>
2016-07-16 08:05:38 -0400

    apply redux devtools for chrome extension

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 import App from './app'
-
-let store = createStore(rootReducer, applyMiddleware(thunk))
+// TODO only add devtools extension when NODE_ENV === 'development'
+let store = createStore(rootReducer, compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+))
 
 const Index = () => {
   return (
