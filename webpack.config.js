5bb5e4612a1c43658b8ddf4ead18525ecfeb036e
jondcoleman <jcoleman3307@gmail.com>
2016-07-22 19:47:38 -0500

    more modifications for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
   devtool: 'source-map',
@@ -27,5 +28,10 @@ module.exports = {
         secure: false
       }
     }
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+    })
+  ]
 }
